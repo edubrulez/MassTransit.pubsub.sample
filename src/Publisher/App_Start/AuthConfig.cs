a48e7f65e323d0ac05bb45729fa05c917ed2d167
Erick Nave <enave@synergydatasystems.com>
2013-06-04 15:33:10 -0400

    appeasing resharper - getting rid of unnecessary using statements

--- src/Publisher/App_Start/AuthConfig.cs
+++ src/Publisher/App_Start/AuthConfig.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Web.WebPages.OAuth;
-using Publisher.Models;
-
-namespace Publisher
+﻿namespace Publisher
 {
 	public static class AuthConfig
 	{
