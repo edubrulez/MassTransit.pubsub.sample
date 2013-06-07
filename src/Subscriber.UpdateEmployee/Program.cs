3431f02d9f3c4912847cd7fcfabd523a8ed65fb4
Erick Nave <enave@synergydatasystems.com>
2013-06-07 16:36:22 -0400

    using structuremap in the console subscriber app

--- src/Subscriber.UpdateEmployee/Program.cs
+++ src/Subscriber.UpdateEmployee/Program.cs
@@ -1,17 +1,10 @@
-﻿using MassTransit;
-
-namespace Subscriber.UpdateEmployee
+﻿namespace Subscriber.UpdateEmployee
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Bus.Initialize(serviceBusConfig =>
-				{
-					serviceBusConfig.UseRabbitMq();
-					serviceBusConfig.ReceiveFrom("rabbitmq://localhost/Subscriber.Console");
-					serviceBusConfig.Subscribe(s => s.Consumer<UpdateEmployeeConsumer>());
-				});
+			IocAbstraction.Bootstrap();
 		}
 	}
 }
