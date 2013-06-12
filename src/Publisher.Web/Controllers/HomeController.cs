ba8c52dee9c4915be3889214871f0683b67a29dc
Erick Nave <enave@synergydatasystems.com>
2013-06-12 11:13:19 -0400

    removed pages/models/controllers from web app since all we need is the simple index page and the 2 simple publish pages.

--- src/Publisher.Web/Controllers/HomeController.cs
+++ src/Publisher.Web/Controllers/HomeController.cs
@@ -10,19 +10,5 @@ namespace Publisher.Controllers
 
 			return View();
 		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your app description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
 	}
 }
